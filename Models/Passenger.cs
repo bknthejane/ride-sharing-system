dc6e9f502ed266f9673a52d5e245453a1f4d832c
Bonolo-Nth <bknthejane@gmail.com>
2025-06-30 12:32:41 +0200

    Added IPayable and IRideable on the Passenger class

--- Models/Passenger.cs
+++ Models/Passenger.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RideSharingSystem.Interfaces;
 
 namespace RideSharingSystem.Models
 {
-    internal class Passenger: User
+    internal class Passenger: User, IPayable, IRideable
     {
         public decimal Wallet { get; set; }
+        public List<Ride> RideHistory { get; set; } = new List<Ride>();
 
         public Passenger(string username, string password) : base(username, password) { }
 
@@ -19,7 +22,15 @@ namespace RideSharingSystem.Models
 
         public void RequestRide(string pickup, string dropoff)
         {
-            
+            var ride = new Ride
+            {
+                Passenger = this,
+                PickupLocation = pickup,
+                DropOffLocation = dropoff,
+                Distance = new Random().Next(1, 20)
+            };
+
+            Console.WriteLine($"Ride requested from {pickup} to {dropoff}, approximate distance {ride.Distance}km.");
         }
     }
 }
