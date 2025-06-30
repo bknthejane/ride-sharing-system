1589fab972a875a458a116bd43aaa535e7b2341e
Bonolo-Nth <bknthejane@gmail.com>
2025-06-30 11:42:56 +0200

    Updated the Driver class

--- Models/Driver.cs
+++ Models/Driver.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace RideSharingSystem.Models
 {
-    internal class Driver
+    internal class Driver: User
     {
+        public Driver(string username, string password) : base(username, password)
+        {
+        }
+
+        public bool Available { get; set; }
+        public decimal Earnings { get; set; }
+        public double Rating { get; set; }
+        public int RidesCompleted { get; set; }
     }
 }
